f7bb02f1484c19d04e334140169651272cbc47e5
mhp4746 <mayank13398@gmail.com>
2026-04-09 16:43:39 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,14 @@
-import { createClient } from '@supabase/supabase-js'
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
-const supabaseUrl = 'https://crinihlzalhwwjhtvvyd.supabase.co'
-const supabaseKey = 'sb_publishable_fd7SxxE_Ps3rNNcfwF3fEg_L1LluPhK'
+const firebaseConfig = {
+  apiKey: "AIzaSyA0wjUH8hB1VfqasPFqON2xBpr_uhWRMpA",
+  authDomain: "pdcd-tracker.firebaseapp.com",
+  projectId: "pdcd-tracker",
+  storageBucket: "pdcd-tracker.firebasestorage.app",
+  messagingSenderId: "724568460018",
+  appId: "1:724568460018:web:a36100c090991bce6e1e6b"
+}
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
